e67a26737ea530ec336f0dd232e9ea406988f35a
mattbarackman <matthew.barackman@gmail.com>
2013-06-26 12:52:50 -0700

    fixed syntax error in question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ActiveRecord::Base
   belongs_to :user
 
-  validates :title :presence => true, 
-                   :uniqueness => true
+  validates :title, :presence => true, 
+                    :uniqueness => true
 
 
 end
