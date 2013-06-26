806e6b340a8318fa4613406c4f98b658c4ea9f5c
Apprentice <apprentice@devbootcamp.com>
2013-06-26 11:22:54 -0700

    wip rspec test for questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,37 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    # @question.user = current_user.id
+    if @question.save
+      redirect_to user_question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe Question do
+
+  let(:title)   {"What is this crazy EE thing here at DBC?"}
+  let(:content) {"I've heard a lot about it but don't really understand what is covered"}
+  let(:bob) {mock(:user, name=>"Bob Marley", :id=> 1)}
+  let(:question) {Question.new(title: title, content: content, user_id: bob.id)}
+
+  describe "#index" do
+    it "should return all questions related to user from database" do
+      visit user_questions_path(:bob)
+      expect(page).to have_content question.title
+    end
+  end      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
