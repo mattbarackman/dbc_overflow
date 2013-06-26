dd6882aeaa3f457a7005b0959024034caafd75f4
Apprentice <apprentice@devbootcamp.com>
2013-06-26 16:15:46 -0700

    Added sessions helper/views/controller and enabled signin, signout, and currentuser

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
-    
   end
 
   def create
-    
+    user = User.find_by_email(params[:session][:email])
+    password = params[:session][:password]
+    if user.authenticate(password)
+      sign_in(user)
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def destroy
-    
+    sign_out
+    redirect_to new_session_path
   end  
 
 end
