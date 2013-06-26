618b4cb9d26ee884795a61bbe1bcb2fe0f8554c9
Apprentice <apprentice@devbootcamp.com>
2013-06-26 10:37:38 -0700

    added folders in spec directory

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+# require_relative 'cookie'
+ 
+describe User do
+ 
+  let(:name)   { "Tom Jones" }
+  let(:email)   { "tom@jones.com" }
+  let(:password)   { "tomtom" }
+  let(:user) { User.new(name: name, email: email, password: password) }
+ 
+  describe "#initialize" do
+    context "with valid input" do
+      end
+    end
+ 
+    context "with invalid input" do
+      end
+    end
+  end
+ 
+end
